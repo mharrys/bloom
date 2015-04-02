88b96a2af2a780dcf3c165aa59f8a2a408d317e7
Mattias Harrysson <mathar@kth.se>
2015-04-02 15:18:12 +0200

    update assets

--- src/assets.hpp
+++ src/assets.hpp
@@ -1,11 +1,22 @@
 #ifndef ASSETS_HPP_INCLUDED
 #define ASSETS_HPP_INCLUDED
 
-#define BLINNPHONG_VS "assets/shaders/blinnphong.vs"
-#define BLINNPHONG_FS "assets/shaders/blinnphong.fs"
-#define COPY_VS       "assets/shaders/copy.vs"
-#define COPY_FS       "assets/shaders/copy.fs"
-#define LUMA_FS       "assets/shaders/luma.fs"
-#define SUZANNE_OBJ   "assets/models/suzanne.obj"
+#define SKYBOX_VS  "assets/shaders/skybox.vs"
+#define SKYBOX_FS  "assets/shaders/skybox.fs"
+#define REFLECT_VS "assets/shaders/reflect.vs"
+#define REFLECT_FS "assets/shaders/reflect.fs"
+
+#define COPY_VS    "assets/shaders/copy.vs"
+#define COPY_FS    "assets/shaders/copy.fs"
+#define LUMA_FS    "assets/shaders/luma.fs"
+#define HBLUR_FS   "assets/shaders/hblur.fs"
+#define VBLUR_FS   "assets/shaders/vblur.fs"
+#define TONEMAP_FS "assets/shaders/tonemap.fs"
+
+#define SUZANNE_OBJ "assets/models/suzanne.obj"
+#define TEAPOT_OBJ  "assets/models/teapot.obj"
+#define SPHERE_OBJ  "assets/models/sphere.obj"
+
+#define UFFIZI_CROSS_HDR "assets/hdr/uffizi_cross.hdr"
 
 #endif
