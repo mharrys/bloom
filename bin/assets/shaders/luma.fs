8bfc47a15d9685fac65d08e370f93d77325baffa
Mattias Harrysson <mathar@kth.se>
2015-04-02 15:23:55 +0200

    remove branching in luma shader

--- bin/assets/shaders/luma.fs
+++ bin/assets/shaders/luma.fs
@@ -1,23 +1,19 @@
 #version 130
 
 uniform vec2 resolution;
-uniform sampler2D color_map;
-uniform float thresh = 0.23;
+uniform sampler2D read;
+
+uniform float thresh = 0.5;
 
 out vec4 frag_color;
 
-float luminace(vec3 color) {
-    return 0.299 * color.r + 0.587 * color.g + 0.114 * color.b;
-}
-
-void main()
-{
+void main(void) {
     vec2 uv = gl_FragCoord.xy / resolution.xy;
-    vec4 texel = texture(color_map, uv);
-    if (luminace(texel.rgb) > thresh) {
-        // mix some color into the bloom instead of just having a 100% white bloom
-        frag_color = vec4(mix(texel, vec4(1.0), 0.8).rgb, 1.0);
-    } else {
-        frag_color = vec4(vec3(0.0), 1.0);
-    }
+    vec4 texel = texture(read, uv);
+
+    vec3 luma_compare = vec3(0.2125, 0.7154, 0.0721);
+    float luma = dot(luma_compare, texel.rgb);
+    luma = max(0.0, luma - thresh);
+
+    frag_color = vec4(texel.rgb * sign(luma), 1.0);
 }
