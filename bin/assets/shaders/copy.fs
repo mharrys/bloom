5b79894604689bb66b8fa587a71fed1609980bf2
Mattias Harrysson <mathar@kth.se>
2015-04-02 15:19:20 +0200

    rename input texture

--- bin/assets/shaders/copy.fs
+++ bin/assets/shaders/copy.fs
@@ -1,12 +1,12 @@
 #version 130
 
 uniform vec2 resolution;
-uniform sampler2D color_map;
+uniform sampler2D read;
 
 out vec4 frag_color;
 
 void main()
 {
     vec2 uv = gl_FragCoord.xy / resolution.xy;
-    frag_color = texture(color_map, uv);
+    frag_color = texture(read, uv);
 }
